73b7c72854f484f104f055beafedb1ac8e637515
ajgonzalezm80-byte <ajgonzalezm80@gmail.com>
2026-03-06 17:43:28 -0400

    cambios en app.py para despliegue en Render

--- proyectoWeb/app.py
+++ proyectoWeb/app.py
@@ -35,6 +35,9 @@ def serve_sw():
 
 
 if __name__ == "__main__":
-    app.run()
+    # Render asigna un puerto automáticamente en la variable PORT
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
     
     
