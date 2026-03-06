1405c6273ce0acbd25394b644ff19a1b24b27efa
ajgonzalezm80-byte <ajgonzalezm80@gmail.com>
2026-03-06 18:22:39 -0400

    cambio en app.py y requirements.txt y .gitignore

--- proyectoWeb/app.py
+++ proyectoWeb/app.py
@@ -5,39 +5,33 @@ from routes.views import views
 import os
 from dotenv import load_dotenv
 
-
-# Carga las variables ANTES de configurar la app
-load_dotenv() 
-#chatgpt
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+# Cargar variables de entorno
+load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
+# Base de datos SQLite
 db_path = os.path.join(basedir, "instance", "tasas.db")
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
-# Registrar blueprints
+# Blueprints
 app.register_blueprint(api)
 app.register_blueprint(views)
 
+# Service worker
 @app.route('/sw.js')
 def serve_sw():
-    # return app.send_static_file('sw.js')
     return send_from_directory(
         os.path.join(app.root_path, 'static', 'js'),
         'sw.js'
     )
 
-
+# Ejecutar servidor
 if __name__ == "__main__":
-    # Render asigna un puerto automáticamente en la variable PORT
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-    
-    
